038ef4c1a4f99883c6b1a8217bb4d7b2911c8baf
sahil <bsahil144@gmail.com>
2024-03-10 20:31:17 +0530

    done

---
+++ first js file/first.js
@@ -0,0 +1,58 @@
+// let a = 5;
+// let b=2;
+// console.log("a =",a, "& b =",b);
+// console.log("a + b =",a+b);
+// console.log("a - b =",a-b);
+// console.log("a * b =",a*b);
+// console.log("a / b =",a/b);
+// console.log("a % b =",a%b);
+// console.log("a ** b =",a**b);
+
+
+
+
+// let a = 6;
+// let b = 4;
+// cond1 = a>b;
+// cond2 = b==4;
+// console.log("cond1 && cond2 = ",cond1&&cond2);
+
+
+// let mode="dark";
+// if(mode==="dark"){
+//     color="black";
+// }
+// else{
+//     color="white"
+// }
+// console.log(color);
+
+// let a=1;
+// if(a%2===0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+
+
+
+// let age=14;
+// ans = age>18 ? "adult":"non adult";
+// console.log(ans);
+
+//switch statement
+const expr = 'Papayas';
+switch (expr) {
+  case 'Oranges':
+    console.log('Oranges are $0.59 a pound.');
+    break;
+  case 'Mangoes':
+  case 'Papayas':
+    console.log('Mangoes and papayas are $2.79 a pound.');
+    // Expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
+
